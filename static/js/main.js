62b89ea44d6bab1eccee06854771bdd4f3e4950b
gojumbos <declan.borcich@tufts.edu>
2024-07-08 16:00:24 -0400

    2

--- static/js/main.js
+++ static/js/main.js
@@ -4,7 +4,7 @@
 // const HOME_URL = 'http://127.0.0.1:5000'
 // const HOME_URL = 'http://127.0.0.1:8000'
 // const HOME_URL = 'https://couponsdomain.com:8000';
-const HOME_URL = 'clownfish-app-8om3z.ondigitalocean.app:8000';
+const HOME_URL = 'clownfish-app-8om3z.ondigitalocean.app';
 
 const DATA_INDICATOR = "data";
 const LOGIN_INDICATOR = "login";
