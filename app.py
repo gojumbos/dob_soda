4cdff5e06aeb9939c96dd459322af34b1088292f
gojumbos <declan.borcich@tufts.edu>
2024-08-15 16:49:33 -0400

    favicon fix- instance path

--- app.py
+++ app.py
@@ -104,7 +104,7 @@ def api_home():  # put application's code here
 
 @app.route('/favicon.ico')
 def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static'),'favicon.ico', mimetype='image/vnd.microsoft.icon')
+    return send_from_directory(os.path.join(app.instance_path, 'static'),'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
 # soda
 @app.route('/api/soda_get_update', methods=['GET'])
